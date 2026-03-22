5aaa5f960b952f18bdab2145054c4d5bac7049e9
longscom90 <longscom90@gmail.com>
2026-03-22 19:36:30 +0900

    Update data.py

--- data.py
+++ data.py
@@ -3,26 +3,38 @@ import pandas as pd
 
 BASE_URL = "https://fapi.binance.com"
 
+def get_all_symbols():
+    url = f"{BASE_URL}/fapi/v1/exchangeInfo"
+    data = requests.get(url).json()
+
+    symbols = []
+
+    for s in data["symbols"]:
+        if s["contractType"] == "PERPETUAL" and s["quoteAsset"] == "USDT":
+            symbols.append(s["symbol"])
+
+    return symbols
+
+
 def get_klines(symbol="BTCUSDT", interval="5m", limit=100):
     url = f"{BASE_URL}/fapi/v1/klines"
-    
+
     params = {
         "symbol": symbol,
         "interval": interval,
         "limit": limit
     }
-    
+
     response = requests.get(url, params=params)
     data = response.json()
-    
+
     df = pd.DataFrame(data, columns=[
         "time", "open", "high", "low", "close", "volume",
         "close_time", "qav", "trades", "taker_base_vol",
         "taker_quote_vol", "ignore"
     ])
-    
-    # 숫자형 변환
+
     df["close"] = df["close"].astype(float)
     df["volume"] = df["volume"].astype(float)
-    
+
     return df
